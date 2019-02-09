cff0fb3508415cf0caf8c3498f81d639f1ffbf99
Jeff Maddox <46329844+MaddoxJeff@users.noreply.github.com>
2019-02-08 19:38:28 -0600

    Update ProblemTen.java

--- Euler_Problems/ProblemTen.java
+++ Euler_Problems/ProblemTen.java
@@ -11,27 +11,28 @@ public class ProblemTen {
 	public static int sumPrimes(ArrayList<Integer> n) {
 		
 		ArrayList<Integer> temp = new ArrayList<Integer>();
-		for (int k = 0; k < 10000; k++) {
+		for (int k = 2; k < 2000000; k++) {
 			if (isPrime(k)==true)
 				temp.add(k);
 		}
 		int x = sum(temp);
 		return x;
 	}
-	
 	public static boolean isPrime(int n) {
 		int counter = 0;
-		for(int i = 0; i < n;i++) {
-			if(i == 0)
-				continue;
-			if(n%i == 0 )
-				counter++;
-		}
-		if (counter == 1)
+		if (n == 2) {
 			return true;
-		else
+		} else if (n % 2 == 0) {
 			return false;
-	}
+		}
+		for(int i = 3; i*i <= n;i++) {
+			if(n%i == 0) {
+				return false;
+			}			
+		}
+		return true;
+	}	
+
 	public static int sum(List<Integer> list) {
 		
 		int sum = 0;
