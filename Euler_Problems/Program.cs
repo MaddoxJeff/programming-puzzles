4a17709921fe576f86773b597a33fa410e83e220
Jeff Maddox <46329844+MaddoxJeff@users.noreply.github.com>
2019-02-12 00:58:01 -0600

    Add files via upload

---
+++ Euler_Problems/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace EulerProb16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = 2;
+            BigInteger exp = 1000;
+            BigInteger k = power(n, exp);
+            Console.WriteLine(n + "^" + exp + ": ");
+            Console.WriteLine(k + " =");
+            Console.WriteLine(sumOfDigits(k));
+        }
+        public static BigInteger power(BigInteger n, BigInteger exp)
+        {
+            BigInteger value = n;
+            while (exp > 1)
+            {
+                value = value * n;
+                exp--;
+            }
+            return value;
+        }
+        public static BigInteger sumOfDigits(BigInteger n)
+        {
+            BigInteger temp = 0;
+            BigInteger len = n.ToString().Length;
+            BigInteger value = 0;
+
+            while (n > 0)
+            {
+                temp = (n % 10);
+                value += temp;
+                n = n / 10;
+            }
+            return value;
+        }
+    }
+}
