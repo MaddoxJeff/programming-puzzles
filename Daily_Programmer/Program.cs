3b41f69abdc3454e63b6068e917d06ed9f46f5d1
Jeff Maddox <46329844+MaddoxJeff@users.noreply.github.com>
2019-02-11 22:51:17 -0600

    Add files via upload

---
+++ Daily_Programmer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Numerics;
+
+namespace subFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = 9;
+            Console.WriteLine("Number: " + n);
+            BigInteger k = factorial(n);
+            double num = convDouble(k);
+            Console.WriteLine("Factorial: " + num);
+            double e = 2.718281828459;
+            double solution = num / e;
+            Console.WriteLine("Derangement: " + Math.Round(solution));
+        }
+        public static BigInteger factorial(BigInteger num)
+        {
+            BigInteger res = 1;
+            while (num != 1)
+            {
+                res = res * num;
+                num = num - 1;
+            }
+            return res;
+        }
+        public static double convDouble(BigInteger n)
+        {
+            BigInteger temp = 0;
+            
+            int len = n.ToString().Length;
+            BigInteger[] sep = new BigInteger[len];
+            int counter = len;
+
+            while (n > 0)
+            {
+                counter--;
+                temp = (n % 10);
+                sep[counter] = temp;
+                n = n / 10;
+            }
+            String res = string.Join("", sep);
+            double d = Convert.ToDouble(res);
+            return d;
+
+        }
+    }
+}
