4e9f606ad669e5b6f118e9c44796f460aae65c98
Jeff Maddox <46329844+MaddoxJeff@users.noreply.github.com>
2019-02-15 13:45:20 -0600

    Update ProblemTwo.java

--- Euler_Problems/Problem2/ProblemTwo.java
+++ Euler_Problems/Problem2/ProblemTwo.java
@@ -1,39 +1,17 @@
-import java.util.*;
-import java.util.ArrayList;
+
 
 public class ProblemTwo {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> test = new ArrayList<Integer>();
-		test = fib(test);
-		System.out.println(even(test));		
-	}
-	public static ArrayList<Integer> fib(ArrayList<Integer> n){
-		
-		int prev = 1, current = 1, next = 1;
-		for (int i = 0; i < 100; i++) {
-			if (i == 0) {
-				n.add(1);
-				continue;
-			}
-			next = prev+current;
-			n.add(next);
-			prev = current;
-			current = next;
+		long[] fib = { 2, 0 };
+		int n = 0;
+		long total = 0;
+		while (fib[n]< 4000000) {
+			total += fib[n];
+			n = (n+1)%2;
+			fib[n] = 4*fib[(n+1)%2]+fib[n];
 		}
-		return n;
-	}
-	public static ArrayList<Integer> even(ArrayList<Integer> n){
-		
-		ArrayList<Integer> temp = new ArrayList<Integer>();
-		for (int i: n) {
-			if (i > 400000000)
-				break;
-			if(i % 2 == 0)
-				temp.add(i);
-		}
-		return temp;
-		
+		System.out.println(total);
 	}
 
 }
