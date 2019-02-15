ba44c667e1d62db0366fc6501da301eb46796c41
Jeff Maddox <46329844+MaddoxJeff@users.noreply.github.com>
2019-02-15 15:34:16 -0600

    Add files via upload

---
+++ Euler_Problems/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            int counter = 0;
+            while(counter < 10001)
+            {
+                n++;
+                if (isPrime(n) == true)
+                    counter++;
+                
+            }
+            Console.WriteLine(n);
+        }
+        public static Boolean isPrime(int n)
+        {
+            int counter = 0;
+            if (n == 2)
+            {
+                return true;
+            }
+            else if (n % 2 == 0)
+            {
+                return false;
+            }
+            for (int i = 3; i * i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
