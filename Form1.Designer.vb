68117347e4891d18d0f4acdaa0c3582f4c7040aa
Jeff Maddox <46329844+MaddoxJeff@users.noreply.github.com>
2019-02-20 15:38:02 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,409 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelX = New System.Windows.Forms.Label()
+        Me.TextBoxX = New System.Windows.Forms.TextBox()
+        Me.LabelY = New System.Windows.Forms.Label()
+        Me.LabelZ = New System.Windows.Forms.Label()
+        Me.TextBoxY = New System.Windows.Forms.TextBox()
+        Me.TextBoxZ = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBoxVol = New System.Windows.Forms.TextBox()
+        Me.TextBoxPrice = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ButtonCalc = New System.Windows.Forms.Button()
+        Me.ButtonPrice = New System.Windows.Forms.Button()
+        Me.RadioButtonA = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonB = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonC = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBoxFName = New System.Windows.Forms.TextBox()
+        Me.TextBoxLName = New System.Windows.Forms.TextBox()
+        Me.TextBoxState = New System.Windows.Forms.TextBox()
+        Me.TextBoxCity = New System.Windows.Forms.TextBox()
+        Me.TextBoxPhone = New System.Windows.Forms.TextBox()
+        Me.ButtonInfo = New System.Windows.Forms.Button()
+        Me.TextBoxInvoice = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LabelX
+        '
+        Me.LabelX.AutoSize = True
+        Me.LabelX.Location = New System.Drawing.Point(12, 72)
+        Me.LabelX.Name = "LabelX"
+        Me.LabelX.Size = New System.Drawing.Size(17, 13)
+        Me.LabelX.TabIndex = 0
+        Me.LabelX.Text = "X:"
+        '
+        'TextBoxX
+        '
+        Me.TextBoxX.Location = New System.Drawing.Point(35, 72)
+        Me.TextBoxX.Name = "TextBoxX"
+        Me.TextBoxX.Size = New System.Drawing.Size(47, 20)
+        Me.TextBoxX.TabIndex = 1
+        '
+        'LabelY
+        '
+        Me.LabelY.AutoSize = True
+        Me.LabelY.Location = New System.Drawing.Point(12, 105)
+        Me.LabelY.Name = "LabelY"
+        Me.LabelY.Size = New System.Drawing.Size(17, 13)
+        Me.LabelY.TabIndex = 2
+        Me.LabelY.Text = "Y:"
+        '
+        'LabelZ
+        '
+        Me.LabelZ.AutoSize = True
+        Me.LabelZ.Location = New System.Drawing.Point(12, 136)
+        Me.LabelZ.Name = "LabelZ"
+        Me.LabelZ.Size = New System.Drawing.Size(17, 13)
+        Me.LabelZ.TabIndex = 3
+        Me.LabelZ.Text = "Z:"
+        '
+        'TextBoxY
+        '
+        Me.TextBoxY.Location = New System.Drawing.Point(35, 105)
+        Me.TextBoxY.Name = "TextBoxY"
+        Me.TextBoxY.Size = New System.Drawing.Size(47, 20)
+        Me.TextBoxY.TabIndex = 4
+        '
+        'TextBoxZ
+        '
+        Me.TextBoxZ.Location = New System.Drawing.Point(35, 133)
+        Me.TextBoxZ.Name = "TextBoxZ"
+        Me.TextBoxZ.Size = New System.Drawing.Size(47, 20)
+        Me.TextBoxZ.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 182)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(172, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Which parts would you like to use?"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 22)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(303, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Please list the dimensions of the widget you would like to order."
+        '
+        'TextBoxVol
+        '
+        Me.TextBoxVol.Location = New System.Drawing.Point(353, 136)
+        Me.TextBoxVol.Name = "TextBoxVol"
+        Me.TextBoxVol.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxVol.TabIndex = 11
+        '
+        'TextBoxPrice
+        '
+        Me.TextBoxPrice.Location = New System.Drawing.Point(353, 182)
+        Me.TextBoxPrice.Name = "TextBoxPrice"
+        Me.TextBoxPrice.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxPrice.TabIndex = 12
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(350, 108)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(42, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Volume"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(350, 166)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(31, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Price"
+        '
+        'ButtonCalc
+        '
+        Me.ButtonCalc.Location = New System.Drawing.Point(211, 105)
+        Me.ButtonCalc.Name = "ButtonCalc"
+        Me.ButtonCalc.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonCalc.TabIndex = 15
+        Me.ButtonCalc.Text = "Calc Vol"
+        Me.ButtonCalc.UseVisualStyleBackColor = True
+        '
+        'ButtonPrice
+        '
+        Me.ButtonPrice.Location = New System.Drawing.Point(211, 262)
+        Me.ButtonPrice.Name = "ButtonPrice"
+        Me.ButtonPrice.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonPrice.TabIndex = 16
+        Me.ButtonPrice.Text = "Calc Price"
+        Me.ButtonPrice.UseVisualStyleBackColor = True
+        '
+        'RadioButtonA
+        '
+        Me.RadioButtonA.AutoSize = True
+        Me.RadioButtonA.Location = New System.Drawing.Point(6, 21)
+        Me.RadioButtonA.Name = "RadioButtonA"
+        Me.RadioButtonA.Size = New System.Drawing.Size(56, 17)
+        Me.RadioButtonA.TabIndex = 20
+        Me.RadioButtonA.TabStop = True
+        Me.RadioButtonA.Text = "PartsA"
+        Me.RadioButtonA.UseVisualStyleBackColor = True
+        '
+        'RadioButtonB
+        '
+        Me.RadioButtonB.AutoSize = True
+        Me.RadioButtonB.Location = New System.Drawing.Point(7, 44)
+        Me.RadioButtonB.Name = "RadioButtonB"
+        Me.RadioButtonB.Size = New System.Drawing.Size(56, 17)
+        Me.RadioButtonB.TabIndex = 21
+        Me.RadioButtonB.TabStop = True
+        Me.RadioButtonB.Text = "PartsB"
+        Me.RadioButtonB.UseVisualStyleBackColor = True
+        '
+        'RadioButtonC
+        '
+        Me.RadioButtonC.AutoSize = True
+        Me.RadioButtonC.Location = New System.Drawing.Point(7, 67)
+        Me.RadioButtonC.Name = "RadioButtonC"
+        Me.RadioButtonC.Size = New System.Drawing.Size(56, 17)
+        Me.RadioButtonC.TabIndex = 22
+        Me.RadioButtonC.TabStop = True
+        Me.RadioButtonC.Text = "PartsC"
+        Me.RadioButtonC.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButtonC)
+        Me.GroupBox1.Controls.Add(Me.RadioButtonB)
+        Me.GroupBox1.Controls.Add(Me.RadioButtonA)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 210)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(112, 110)
+        Me.GroupBox1.TabIndex = 23
+        Me.GroupBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(10, 333)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(174, 13)
+        Me.Label1.TabIndex = 24
+        Me.Label1.Text = "Please enter your information below"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 366)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 25
+        Me.Label2.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 395)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 26
+        Me.Label3.Text = "Last Name"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 420)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(32, 13)
+        Me.Label8.TabIndex = 27
+        Me.Label8.Text = "State"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 444)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(24, 13)
+        Me.Label9.TabIndex = 28
+        Me.Label9.Text = "City"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(12, 472)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(38, 13)
+        Me.Label10.TabIndex = 29
+        Me.Label10.Text = "Phone"
+        '
+        'TextBoxFName
+        '
+        Me.TextBoxFName.Location = New System.Drawing.Point(99, 366)
+        Me.TextBoxFName.Name = "TextBoxFName"
+        Me.TextBoxFName.Size = New System.Drawing.Size(67, 20)
+        Me.TextBoxFName.TabIndex = 30
+        '
+        'TextBoxLName
+        '
+        Me.TextBoxLName.Location = New System.Drawing.Point(99, 392)
+        Me.TextBoxLName.Name = "TextBoxLName"
+        Me.TextBoxLName.Size = New System.Drawing.Size(67, 20)
+        Me.TextBoxLName.TabIndex = 31
+        '
+        'TextBoxState
+        '
+        Me.TextBoxState.Location = New System.Drawing.Point(99, 418)
+        Me.TextBoxState.Name = "TextBoxState"
+        Me.TextBoxState.Size = New System.Drawing.Size(67, 20)
+        Me.TextBoxState.TabIndex = 32
+        '
+        'TextBoxCity
+        '
+        Me.TextBoxCity.Location = New System.Drawing.Point(99, 446)
+        Me.TextBoxCity.Name = "TextBoxCity"
+        Me.TextBoxCity.Size = New System.Drawing.Size(67, 20)
+        Me.TextBoxCity.TabIndex = 33
+        '
+        'TextBoxPhone
+        '
+        Me.TextBoxPhone.Location = New System.Drawing.Point(99, 472)
+        Me.TextBoxPhone.Name = "TextBoxPhone"
+        Me.TextBoxPhone.Size = New System.Drawing.Size(67, 20)
+        Me.TextBoxPhone.TabIndex = 34
+        '
+        'ButtonInfo
+        '
+        Me.ButtonInfo.Location = New System.Drawing.Point(234, 410)
+        Me.ButtonInfo.Name = "ButtonInfo"
+        Me.ButtonInfo.Size = New System.Drawing.Size(158, 47)
+        Me.ButtonInfo.TabIndex = 35
+        Me.ButtonInfo.Text = "Update Database"
+        Me.ButtonInfo.UseVisualStyleBackColor = True
+        '
+        'TextBoxInvoice
+        '
+        Me.TextBoxInvoice.Location = New System.Drawing.Point(99, 498)
+        Me.TextBoxInvoice.Name = "TextBoxInvoice"
+        Me.TextBoxInvoice.Size = New System.Drawing.Size(67, 20)
+        Me.TextBoxInvoice.TabIndex = 36
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(12, 501)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(53, 13)
+        Me.Label11.TabIndex = 37
+        Me.Label11.Text = "InvoiceID"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(535, 538)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.TextBoxInvoice)
+        Me.Controls.Add(Me.ButtonInfo)
+        Me.Controls.Add(Me.TextBoxPhone)
+        Me.Controls.Add(Me.TextBoxCity)
+        Me.Controls.Add(Me.TextBoxState)
+        Me.Controls.Add(Me.TextBoxLName)
+        Me.Controls.Add(Me.TextBoxFName)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ButtonPrice)
+        Me.Controls.Add(Me.ButtonCalc)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBoxPrice)
+        Me.Controls.Add(Me.TextBoxVol)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBoxZ)
+        Me.Controls.Add(Me.TextBoxY)
+        Me.Controls.Add(Me.LabelZ)
+        Me.Controls.Add(Me.LabelY)
+        Me.Controls.Add(Me.TextBoxX)
+        Me.Controls.Add(Me.LabelX)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LabelX As Label
+    Friend WithEvents TextBoxX As TextBox
+    Friend WithEvents LabelY As Label
+    Friend WithEvents LabelZ As Label
+    Friend WithEvents TextBoxY As TextBox
+    Friend WithEvents TextBoxZ As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBoxVol As TextBox
+    Friend WithEvents TextBoxPrice As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ButtonCalc As Button
+    Friend WithEvents ButtonPrice As Button
+    Friend WithEvents RadioButtonA As RadioButton
+    Friend WithEvents RadioButtonB As RadioButton
+    Friend WithEvents RadioButtonC As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBoxFName As TextBox
+    Friend WithEvents TextBoxLName As TextBox
+    Friend WithEvents TextBoxState As TextBox
+    Friend WithEvents TextBoxCity As TextBox
+    Friend WithEvents TextBoxPhone As TextBox
+    Friend WithEvents ButtonInfo As Button
+    Friend WithEvents TextBoxInvoice As TextBox
+    Friend WithEvents Label11 As Label
+End Class
