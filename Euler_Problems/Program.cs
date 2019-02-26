d35d5f759a970159cc7704e90726e4e680eeac6f
Jeff Maddox <46329844+MaddoxJeff@users.noreply.github.com>
2019-02-25 19:39:20 -0600

    Add files via upload

---
+++ Euler_Problems/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Euler20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger n = 100;
+            n = factorial(n);
+            Console.WriteLine(factDigit(n));
+        }
+        public static BigInteger factorial(BigInteger n)
+        {
+            BigInteger res = 1;
+            while (n != 1)
+            {
+                res = res * n;
+                n = n - 1;
+            }
+            return res;
+        }
+        public static int factDigit(BigInteger n)
+        {
+            int[] digits = n.ToString().ToCharArray().Select(x => (int)Char.GetNumericValue(x)).ToArray();
+            int num = 0;
+            for (int i = 0; i < digits.Length; i++)
+            {
+                num += digits[i];
+            }
+            return num;
+        }
+    }
+}
